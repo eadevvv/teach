37f63990c58e277e356caf273945a64deffc60af
Andrew <andrew.ezerski@gmail.com>
2021-09-24 17:28:03 +0700

    Add files via upload
    04-board

--- app.js
+++ app.js
@@ -1,44 +1,58 @@
-const container = document.querySelector('.container')
-const upBtn = document.querySelector('.up-button')
-const downBtn = document.querySelector('.down-button')
+const board = document.querySelector('#board')
+const colors = ['#04d9ff', '#39ff14', '#bc13fe', '#cfff04', '#fe59c2']
+const figureTypes = ['circle', 'square']
+const figureQuantity = [200, 300, 400, 500]
 
-const sidebar = document.querySelector('.sidebar')
-const mainSlide = document.querySelector('.main-slide')
+setRandomBoard() // click on figure to random board
 
-const slidesCount = mainSlide.querySelectorAll('div').length
+function activeBoard(figureType, number) {
+    board.innerHTML = ''
+    for (let i = 0; i < number; i++) {
+        const figure = document.createElement('div')
+        figure.classList.add(figureType)
+    
+        figure.addEventListener('mouseover', () => {
+            setColor(figure)
+        })
+    
+        figure.addEventListener('mouseleave', () => {
+            removeColor(figure)
+        })
 
-let activeSlideIndex = 0
-
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
-
-upBtn.addEventListener('click', () => {
-    changeSlide('up')
-})
-
-downBtn.addEventListener('click', () => {
-    changeSlide('down')
-})
-
-function changeSlide(direction) {
-    if (direction === 'up') {
-        activeSlideIndex++
-        if (activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0
-        }
-    } else if (direction === 'down') {
-        activeSlideIndex--
-        if (activeSlideIndex < 0) {
-            activeSlideIndex = slidesCount - 1
-        }
+        figure.addEventListener('click', () => {
+            const arr = getRandomBoard()
+            activeBoard(arr[0], arr[1])
+        })
+    
+        board.append(figure)
     }
-
-    const height = container.clientHeight
-
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
 }
 
-setInterval(() => {
-    changeSlide('up')
-}, 3000)
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 20px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#dfdfdf'
+    element.style.boxShadow = `0 0 2px #fff`
+}
+
+function getRandomColor() {
+    const i = Math.floor(Math.random() * colors.length)
+
+    return colors[i]
+}
+
+function getRandomBoard() {
+    const type = Math.floor(Math.random() * figureTypes.length)
+    const quantity = Math.floor(Math.random() * figureQuantity.length)
+
+    return Array(figureTypes[type], figureQuantity[quantity])
+}
+
+function setRandomBoard() {
+    const arr = getRandomBoard()
+    activeBoard(arr[0], arr[1])
+}
